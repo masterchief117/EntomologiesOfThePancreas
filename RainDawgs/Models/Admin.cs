d02e18a8b5044c37eaa520cbf0dcac307f6ddd0d
masterchief117 <bstewart@catalystitservices.com>
2013-03-13 11:14:36 -0700

    First Commits

---
+++ RainDawgs/Models/Admin.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using BaseBallClub.Interface;
+
+namespace RainDawgs.Models
+{
+    public class Admin : IUser
+    {
+
+    }
+}
---
+++ RainDawgs/Models/Interface.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BaseBallClub.Interface
+{
+    interface IUser
+    {
+        private string username;
+        private string password;
+        private string firstName;
+        private string lastName;
+        private string role;
+    }
+
+    interface IRegion
+    {
+        private int regionId;
+    }
+}
