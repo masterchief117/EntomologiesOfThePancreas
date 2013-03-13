383aed83d38b9eae263d55407a27567b1b329868
masterchief117 <bstewart@catalystitservices.com>
2013-03-13 16:00:17 -0700

    AMessage

---
+++ BaseballCardsMVC/Models/UserFactory.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BaseballCardsMVC.Models
+{
+    public abstract class UserFactory
+    {
+        public string username {get; set;}
+        public string password { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string role { get; set; }
+    }
+
+    public class Region {
+        public int id { get; set; }
+        public string name { get; set; }
+        public decimal discountRate {get; set;}
+    }
+
+    public abstract class Store {
+        public int id { get; set; }
+        public int regionId { get; set; }
+        public decimal discountRate { get; set; }
+    }
+
+    public class HRManager : UserFactory { }
+
+    public class SuperAdmin : UserFactory { }
+    public class TechAdmin : UserFactory { }
+    public class RegionalManager : UserFactory {
+        public int regionId { get; set; }
+    }
+
+    public class StoreManager : UserFactory {
+        public int storeId { get; set; }
+    }
+
+    public class Member : UserFactory {
+        public int regionId { get; set; }
+    }
+    public class Dependent: UserFactory {
+        public int owningMemberId { get; set; }
+    }
+}
